b5c8440a7459c44758f0566a918b3f0511f2913d
NithikornJ <nithikorn.jst@gmail.com>
2025-06-18 00:31:12 +0700

    update link

--- src/data/projects.js
+++ src/data/projects.js
@@ -12,7 +12,7 @@ export const projects = [
     description: "เว็บแสดงโปรไฟล์และโปรเจกต์ของตัวเองด้วย React + Tailwind",
     techs: ["React", "Tailwind", "DaisyUI"],
     image: "/projects/portfolio.png",
-    // link: "https://lawsearch.nithikorn.dev",
+    link: "https://nithikorn-portfolio.netlify.app/",
     github: "https://github.com/NithikornJ/nithikorn-portfolio",
     category: "frontend",
   },
